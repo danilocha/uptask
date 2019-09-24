34f2f8069fd823e737b1493dad8830452c091417
daniel lopez <danilocha1@gmail.com>
2019-09-24 09:05:25 -0500

    cambiar servidor smtp pulse.com

--- config/email.js
+++ config/email.js
@@ -1,6 +1,6 @@
 module.exports = {
-  user: "postmaster@sandboxc6e2274d37c74ab9b7aa08faddf1b517.mailgun.org",
-  pass: "f03ea6cc7f3788ddf47246101eb3d0af-baa55c84-8c51795e",
-  host: "smtp.mailgun.org",
-  port: "587"
+  user: "danilocha1@gmail.com",
+  pass: "De8NLDJgPr",
+  host: "smtp-pulse.com",
+  port: "	2525"
 };
