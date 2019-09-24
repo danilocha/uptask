dd9b305d4c3a214acf64ba8176ca06ac23a311a3
daniel lopez <danilocha1@gmail.com>
2019-09-24 08:49:43 -0500

    cambiar servidor smtp

--- config/email.js
+++ config/email.js
@@ -1,6 +1,6 @@
 module.exports = {
-  user: "f2a56423b5aee5",
-  pass: "82991ce36df852",
-  host: "smtp.mailtrap.io",
-  port: "2525"
+  user: "postmaster@sandboxc6e2274d37c74ab9b7aa08faddf1b517.mailgun.org",
+  pass: "f03ea6cc7f3788ddf47246101eb3d0af-baa55c84-8c51795e",
+  host: "smtp.mailgun.org",
+  port: "587"
 };
